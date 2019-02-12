4c5e6567e527b75c9069e79351558bcee9331170
Ivan De Martino <ivan_demartino@outlook.com>
2019-02-12 15:50:10 -0200

    (#2) Criacao do provider protocol

---
+++ OpenWeatherApp/Providers/CityProviderType.swift
@@ -0,0 +1,13 @@
+//
+//  CityProviderType.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 2/12/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+
+import Foundation
+
+protocol CityProviderType {
+  func fetch(with searchTerm: String, completion: @escaping (City) -> ())
+}
