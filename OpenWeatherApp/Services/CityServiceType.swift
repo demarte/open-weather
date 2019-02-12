140458a3fc5319506b6fbe88ca188cf485ca3f5d
Ivan De Martino <ivan_demartino@outlook.com>
2019-02-12 15:51:47 -0200

    (#2) Criacao do service protocol

---
+++ OpenWeatherApp/Services/CityServiceType.swift
@@ -0,0 +1,13 @@
+//
+//  CityServiceType.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 2/12/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+
+import Foundation
+
+protocol CityServiceType {
+  func fetch(with searchTerm: String, completion: @escaping (City) -> ())
+}
