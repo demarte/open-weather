aa95ce31d3fc9a3d3d47d616a283bf06b3f8a3f7
Ivan De Martino <ivan_demartino@outlook.com>
2019-02-12 15:47:42 -0200

    (#2) Criacao das structs City e Weather

---
+++ OpenWeatherApp/Models/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 2/12/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+
+import Foundation
+
+struct City: Codable {
+  
+  let name: String
+  let weather: [Weather]
+  
+}
---
+++ OpenWeatherApp/Models/Weather.swift
@@ -0,0 +1,17 @@
+//
+//  Weather.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 2/12/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+
+import Foundation
+
+struct Weather: Codable {
+  
+  let main: String
+  let description: String
+  let icon: String
+  
+}
