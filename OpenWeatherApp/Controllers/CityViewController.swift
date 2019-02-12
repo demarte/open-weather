100d8a2dcc74eaccf3e1344db008774a1e1d3e08
Ivan De Martino <ivan_demartino@outlook.com>
2019-02-12 16:17:02 -0200

    (#2) Criacao do controller de cidade para testar o service

---
+++ OpenWeatherApp/Controllers/CityViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CityViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 2/12/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+  
+  private let cityService: CityServiceType = CityServiceRestAPI()
+  private let searchTerm = "Zihuatanejo"
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    cityService.fetch(with: searchTerm) { (city) in
+      print("City json: \(city)")
+    }
+  }
+  
+}
