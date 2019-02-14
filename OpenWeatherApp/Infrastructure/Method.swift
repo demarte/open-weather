3087de4e325022501b3bfb47f913ee68fc7e652a
Ivan De Martino <ivan_demartino@outlook.com>
2019-02-14 11:46:22 -0200

    (#2) Criacao do enum de metodos HTTP

---
+++ OpenWeatherApp/Infrastructure/Method.swift
@@ -0,0 +1,14 @@
+//
+//  Method.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 2/14/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+  case post = "POST"
+  case get = "GET"
+}
