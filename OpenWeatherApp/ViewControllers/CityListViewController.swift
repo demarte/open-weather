aff77a738c08c36feb0510c84071f88c151e9114
Ivan De Martino <ivan_demartino@outlook.com>
2019-02-19 17:19:25 -0300

    (#6) Criacao do CityListController para inicializar a aplicacao caso o usuario tenha permitido a localizacao

---
+++ OpenWeatherApp/ViewControllers/CityListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CityListViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 2/19/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+
+import UIKit
+
+class CityListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
