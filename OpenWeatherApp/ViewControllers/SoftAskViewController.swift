7a51748e9e32b78ddc45989ed7df36db99c73ba5
Ivan De Martino <ivan_demartino@outlook.com>
2019-03-06 04:46:33 -0300

    (#12) Alteracao nos Markers

--- OpenWeatherApp/ViewControllers/SoftAskViewController.swift
+++ OpenWeatherApp/ViewControllers/SoftAskViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 final class SoftAskViewController: UIViewController {
-  // MARK: Properties
+  // MARK: - Properties
   private var locationService: LocationManagerServiceType?
 
   private let stackView = UIStackView(frame: .zero)
@@ -47,7 +47,7 @@ final class SoftAskViewController: UIViewController {
     button.setTitleColor(.silver, for: .normal)
     return button
   }()
-  // MARK: Initializers
+  // MARK: - Initializers
   init(locationService: LocationManagerServiceType) {
     super.init(nibName: nil, bundle: nil)
     self.locationService = locationService
@@ -68,7 +68,7 @@ final class SoftAskViewController: UIViewController {
     setUpView()
     setUpStackView()
   }
-  // MARK: Set up view and subViews
+  // MARK: - Set up view and subViews
   private func setUpView() {
     view.backgroundColor = .white
     view.layer.borderWidth = Constants.borderWidth
@@ -92,7 +92,7 @@ final class SoftAskViewController: UIViewController {
         -Constants.stackViewSpacingToView)
       ])
   }
-  // MARK: Functions
+  // MARK: - Functions
   @objc private func handleYes() {
     locationService?.requestWhenInUseAuthorization(completion: {
       self.exit()
@@ -107,7 +107,7 @@ final class SoftAskViewController: UIViewController {
     self.dismiss(animated: true, completion: nil)
   }
 }
-// MARK: Constants
+// MARK: - Constants
 extension SoftAskViewController {
   private struct Constants {
     static let borderWidth: CGFloat = 20.0
@@ -117,7 +117,7 @@ extension SoftAskViewController {
     static let stackViewSpacing: CGFloat = 20.0
   }
 }
-// MARK: Custom colors
+// MARK: - Custom colors
 extension UIColor {
   static let silver: UIColor = UIColor(named: "Silver") ?? #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
 }
