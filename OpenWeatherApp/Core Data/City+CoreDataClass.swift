ba82dbc9b8da216fe74e577942a2033e41051bca
Ivan De Martino <ivan_demartino@outlook.com>
2019-03-06 04:42:54 -0300

    (#12) Criacao do model do Core Data para persistencia

---
+++ OpenWeatherApp/Core Data/City+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  City+CoreDataClass.swift
+//  OpenWeatherApp
+//
+//  Created by Ivan De Martino on 3/5/19.
+//  Copyright © 2019 Ivan De Martino. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+public class CityTest: NSManagedObject {
+
+}
